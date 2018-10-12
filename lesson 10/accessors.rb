ae1f4214b85e899d92f6f41f1a84f56bb1c54099
v3rba <verbush@gmail.com>
2018-10-12 12:34:00 +0300

    Add method strong_attr_accessor

--- lesson 10/accessors.rb
+++ lesson 10/accessors.rb
@@ -5,4 +5,13 @@ module Accessors
         define_methods(name)
     end
   end
+
+  def strong_attr_accessor(name, type)
+      sym_name = "@#{name}".to_sym
+      define_method(name) { instance_variable_get sym_name }
+      define_method("#{name}=") do |value|
+        raise "Type should be #{type}" unless type == value.class
+        instance_variable_set sym_name, value
+      end
+    end
 end
