1eee612f1ef4ffffe15d3d73023aa281da88cace
v3rba <verbush@gmail.com>
2018-10-15 20:57:53 +0300

    Add module for InstanceMethods

--- lesson 10/accessors.rb
+++ lesson 10/accessors.rb
@@ -8,11 +8,38 @@ module Accessors
   end
 
   def strong_attr_accessor(name, type)
-      sym_name = "@#{name}".to_sym
-      define_method(name) { instance_variable_get sym_name }
-      define_method("#{name}=") do |value|
-        raise "Type should be #{type}" unless type == value.class
-        instance_variable_set sym_name, value
-      end
+    sym_name = "@#{name}".to_sym
+    define_method(name) { instance_variable_get sym_name }
+    define_method("#{name}=") do |value|
+      raise "Type should be #{type}" unless type == value.class
+      instance_variable_set sym_name, value
+    end
   end
+
+  def define_methods(name)
+    sym_name = "@#{name}".to_sym
+    sym_history = "@#{name}_history".to_sym
+    define_method(name) { instance_variable_get sym_name }
+    define_method("#{name}=") do |value|
+      instance_variable_set(sym_history, []) if empty_history?(sym_history)
+      instance_variable_get(sym_history).send(:<<, value)
+      instance_variable_set sym_name, value
+    end
+    define_method("#{name}_history") { instance_variable_get(sym_history) }
+  end
+  end
+
+  module InstanceMethods
+
+    def empty_history?(sym_history)
+      instance_variable_get(sym_history).nil?
+    end
+
+  end
+
+  def self.included(receiver)
+    receiver.send :include, InstanceMethods
+    receiver.extend         ClassMethods
+  end
+
 end
