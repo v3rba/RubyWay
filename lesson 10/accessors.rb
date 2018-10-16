9c1779751212f6596836c85353f7870ac6f19993
v3rba <verbush@gmail.com>
2018-10-16 10:15:28 +0300

    Accessors fix

--- lesson 10/accessors.rb
+++ lesson 10/accessors.rb
@@ -1,35 +1,42 @@
 module Accessors
 
   module ClassMethods
+
     def attr_accessor_with_history(*names)
       names.each do |name|
         define_methods(name)
-    end
-  end
+      end
 
-  def strong_attr_accessor(name, type)
-    sym_name = "@#{name}".to_sym
-    define_method(name) { instance_variable_get sym_name }
-    define_method("#{name}=") do |value|
-      raise "Type should be #{type}" unless type == value.class
-      instance_variable_set sym_name, value
     end
-  end
 
-  def define_methods(name)
-    sym_name = "@#{name}".to_sym
-    sym_history = "@#{name}_history".to_sym
-    define_method(name) { instance_variable_get sym_name }
-    define_method("#{name}=") do |value|
-      instance_variable_set(sym_history, []) if empty_history?(sym_history)
-      instance_variable_get(sym_history).send(:<<, value)
-      instance_variable_set sym_name, value
+    def strong_attr_accessor(name, type)
+      sym_name = "@#{name}".to_sym
+      define_method(name) { instance_variable_get sym_name }
+      define_method("#{name}=") do |value|
+        raise "Type should be #{type}" unless type == value.class
+        instance_variable_set sym_name, value
+      end
     end
-    define_method("#{name}_history") { instance_variable_get(sym_history) }
-  end
+
+    protected
+
+    def define_methods(name)
+      sym_name = "@#{name}".to_sym
+      sym_history = "@#{name}_history".to_sym
+      define_method(name) { instance_variable_get sym_name }
+      define_method("#{name}=") do |value|
+        instance_variable_set(sym_history, []) if empty_history?(sym_history)
+        instance_variable_get(sym_history).send(:<<, value)
+        instance_variable_set sym_name, value
+      end
+      define_method("#{name}_history") { instance_variable_get(sym_history) }
+    end
+
   end
 
   module InstanceMethods
+
+    protected
 
     def empty_history?(sym_history)
       instance_variable_get(sym_history).nil?
